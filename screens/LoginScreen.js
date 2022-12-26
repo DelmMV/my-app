6d6c643ae0b065c19a36ca73df3755da06b9ae29
DelmMV <opi588@yandex.ru>
2022-12-26 21:06:39 +0300

    add: Стилизовать логинскрин

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -32,19 +32,25 @@ export default function LoginScreen({ navigation }) {
   };
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Привет!</Text>
+      <Text style={styles.text}>Служба доставки ЦЕХ85</Text>
       <TextInput
+        style={[styles.input, styles.shadowProp]}
         placeholder="Login"
+        placeholderTextColor="#fff"
         value={login}
         onChangeText={(text) => setlogin(text)}
       ></TextInput>
       <TextInput
+        style={styles.input}
         placeholder="Password"
+        placeholderTextColor="#fff"
         value={password}
         onChangeText={(text) => setPassword(text)}
       ></TextInput>
       <TouchableOpacity>
-        <Button title="Login" onPress={handleLogin}></Button>
+        <Text style={styles.button} onPress={handleLogin}>
+          Войти
+        </Text>
       </TouchableOpacity>
     </View>
   );
@@ -52,12 +58,43 @@ export default function LoginScreen({ navigation }) {
 
 const styles = StyleSheet.create({
   container: {
+    flex: 1,
     paddingTop: 150,
     alignItems: "center",
+    backgroundColor: "#17212b",
+    color: "red",
   },
   text: {
-    color: "black",
+    color: "#fff",
     fontWeight: "bold",
     fontSize: 15,
+    marginBottom: 10,
+  },
+  input: {
+    color: "white",
+    width: 150,
+    margin: 10,
+    borderWidth: 1,
+    borderColor: "red",
+    textAlign: "center",
+    borderRadius: 10,
+  },
+  shadowProp: {
+    shadowOffset: { width: 3, height: 4 },
+    shadowColor: "#black",
+    shadowOpacity: 0.2,
+    shadowRadius: 3,
+  },
+
+  button: {
+    color: "#fff",
+    marginTop: 10,
+    width: 70,
+    height: 40,
+    borderWidth: 1,
+    borderRadius: 10,
+    textAlign: "center",
+    textAlignVertical: "center",
+    backgroundColor: "green",
   },
 });
