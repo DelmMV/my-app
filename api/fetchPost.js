1d16cf46447fee5209eaad8e65b4f718cce6371b
DelmMV <opi588@yandex.ru>
2023-01-14 16:57:06 +0300

    add Добавил фильтр заказов

--- api/fetchPost.js
+++ api/fetchPost.js
@@ -1,27 +1,6 @@
 import ApiManager from "./ApiManager";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
-// export async function featchPost() {
-//   try {
-//     const response = await fetch(
-//       `https://app.tseh85.com/Service/api/delivery/orders?LastUpdateTicks=0`,
-//       {
-//         method: "GET",
-//         headers: {
-//           "content-type": "application/json",
-//           token:
-//             "JQlHiiKvk8zs06L9i9Z5Egr1yFPeK6kHp05lTFyAQRiyyzBkYQR5u7/N0+xZ4YrU",
-//         },
-//       }
-//     );
-//     console.log(response.json());
-//     return response.ok ? await response.json() : [];
-//   } catch (e) {
-//     console.log(e);
-//     return [];
-//   }
-// }
-
 export async function featchPost() {
   const dataToken = await AsyncStorage.getItem("AccessToken");
   try {
